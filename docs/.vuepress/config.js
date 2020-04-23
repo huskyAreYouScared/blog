9736229c356c3ada7eafb0ca95398b437689f3ff
huskyareyouscared <1710632585@qq.com>
2020-04-23 21:20:11 +0800

    add pwd plugins

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,17 @@ module.exports = {
   markdown: {
     lineNumbers: true
   },
+  head: [
+    ['link', { rel: 'icon', href: '/logo.jpg' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
   base:'/blog/',
   dest:'dist',
   title: 'huskyAreYouScared-Blog',
@@ -79,5 +90,9 @@ module.exports = {
       }
     }
   },
-  plugins: ['@vuepress/back-to-top', '@vuepress/active-header-links','@vuepress/nprogress']
+  plugins: ['@vuepress/back-to-top', '@vuepress/active-header-links','@vuepress/nprogress',
+    '@vuepress/pwa',{
+      serviceWorker: true,
+      updatePopup: true
+    }]
 }
