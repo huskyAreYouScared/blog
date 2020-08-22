610e1a5123456eee7fed62d934f72a639a64b978
huskyareyouscared <1710632585@qq.com>
2020-08-23 02:39:28 +0800

    optimize minimap component

--- docs/.vuepress/components/Minimap.vue
+++ docs/.vuepress/components/Minimap.vue
@@ -9,25 +9,17 @@ export default {
   },
   methods: {
     initValine () {
-        // 建立脚本以及属性
         const minimap = document.createElement('script');
         minimap.type = 'text/javascript';
-        // minimap.async = true;
-        // minimap.crossorigin = 'anonymous';
         minimap.src = 'https://cdn.jsdelivr.net/npm/pagemap@1.4.0/dist/pagemap.min.js';
         window.document.body.appendChild(minimap);
-        minimap.onload=function(){
-          pagemap(document.querySelector('#minimap'),{
+        minimap.onload = () => {
+          pagemap(document.querySelector('#minimap'), {
             styles: {
-            'header,footer,section,article': 'rgba(0,0,0,0.08)',
-            'h1': 'rgba(0,255,255,0.20)',
-            'a': 'rgba(255,255,0,0.50)',
-            'h2,h3,h4': 'rgba(0,0,255,0.30)',
-            'pre,code':'rgba(0,255,0,0.1)'
-            },
-            back: 'rgba(255,255,255,0.1)'
+              'pre,code':'rgba(0,255,0,0.1)'
+            }
         });
-        }
+      }
     }
   },
   mounted: function(){
@@ -50,5 +42,4 @@ export default {
     display: none;
   }
 }
-
 </style>
